4da2e4758b6547ade277d8b6ea186dee2df8f0e3
Andrei Macovei <48647892+AndreiM13@users.noreply.github.com>
2025-05-03 22:03:42 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import os
 from run import app  # Import your Flask app from run.py
 
 # Add the directory where run.py is located to the system path
-sys.path.insert(0, '/home/yourusername/yourprojectfolder')  # Adjust this to your actual path
+sys.path.insert(0, '/home/AndreiM13/Tour')  # Adjust this to your actual path
 
 # Use this line to define your application for PythonAnywhere
 application = app
