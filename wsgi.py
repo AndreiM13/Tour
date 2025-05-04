b3b85399701931660c981b36755522c3428eeca4
Andrei Macovei <48647892+AndreiM13@users.noreply.github.com>
2025-05-04 22:04:20 +0100

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-from run import app  # Import your Flask app from run.py
-
-# Add the directory where run.py is located to the system path
-sys.path.insert(0, '/home/AndreiM13/Tour')  # Adjust this to your actual path
-
-# Use this line to define your application for PythonAnywhere
-application = app
